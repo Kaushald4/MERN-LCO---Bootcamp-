db0bc3223b23055125daa7845096107e404482a5
Kaushal Mehta <kaushalkumar304@gmail.com>
2020-06-18 20:48:19 +0530

    adding environment variable dotenv and also adding signout routes and signout controllers only boilerplate

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const port = 8000;
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const port = process.env.PORT || 8000;
 
+//Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const authRoutes = require("./routes/auth");
+
+//DB Connections
 mongoose
-  .connect("mongodb://127.0.0.1:27017/tshirt", {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -13,9 +25,13 @@ mongoose
     console.log("DB Connected....");
   })
   .catch((error) => {
-    console.log(error);
+    console.log("DB GOT OPPS!", error);
   });
 
+//My Routes
+app.use("/api", authRoutes);
+
+// Starting server
 app.listen(port, () => {
   console.log(`app is running at ${port}`);
 });
