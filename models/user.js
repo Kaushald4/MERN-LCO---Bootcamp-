addf895d4c0ca4572b1d85962cb2201bb282c296
Kaushal Mehta <kaushalkumar304@gmail.com>
2020-06-18 22:03:23 +0530

    adding user signup functionality and validation with express-validator

--- models/user.js
+++ models/user.js
@@ -1,73 +1,76 @@
-const mongoose = require('mongoose');
-const crypto = require('crypto');
-const { v1: uuidv1 } = require('uuid');
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const { v1: uuidv1 } = require("uuid");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        maxlength: 32,
-        trim: true
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true,
     },
     lastname: {
-        type: String,
-        maxlength: 32,
-        trim: true
+      type: String,
+      maxlength: 32,
+      trim: true,
     },
 
     email: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
     },
     userinfo: {
-        type: true,
-        trim: true
+      type: String,
+      trim: true,
     },
     encry_password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     salt: String,
 
     role: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     purchases: {
-        type: Array,
-        default: []
-    }
-}, {timestamps: true})
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
 
-userSchema.virtual('password')
-    .set(function(password){
-        this._password = password
-        this.salt = uuidv1()
-        this.encry_password = this.securePassword(password)
-    })
-    .get(function() {
-        return this._password
-    })
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.encry_password = this.securePassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
 
 userSchema.method({
-    authenticate: function(plainPassword) {
-        return this.securePassword(plainPassword) === this.encry_password
-    },
-    securePassword: function(plainPassword) {
-        if(!plainPassword) return ''
-        try {
-            return crypto.createHmac('sha256', this.salt)
-            .update(plainPassword)
-            .digest('hex')
-        } catch (error) {
-            return ''
-        }
+  authenticate: function (plainPassword) {
+    return this.securePassword(plainPassword) === this.encry_password;
+  },
+  securePassword: function (plainPassword) {
+    if (!plainPassword) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainPassword)
+        .digest("hex");
+    } catch (error) {
+      return "";
     }
-})
+  },
+});
 
-
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 const { signout } = require("../controllers/auth");
+const { signup } = require("../controllers/auth");
+const { check } = require("express-validator");
 
+router.post(
+  "/signup",
+  [
+    check("name")
+      .isLength({ min: 3 })
+      .withMessage("name should be at least 3 char"),
+    check("email").isEmail(),
+    check("password")
+      .isLength({ min: 3 })
+      .withMessage("password should be atleast 3 char"),
+  ],
+  signup
+);
 router.get("/signout", signout);
 
 module.exports = router;
